9085537b499fc0e660449dd98f5693f4ebfb62c8
Enes KOC <eneskoc.0063@gmail.com>
2024-10-20 15:43:40 +0300

    Library added

---
+++ parallel_programing/src/main/java/com/mycompany/parallel_programing/Parallel_programing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parallel_programing;
+
+/**
+ *
+ * @author Enes
+ */
+public class Parallel_programing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ parallel_programing/src/main/java/com/mycompany/parallel_programing/Pipeline.java
@@ -0,0 +1,18 @@
+import java.util.Properties;
+
+/**
+ *
+ * @author Enes
+ */
+public class Pipeline {
+    
+    private static Properties properties;
+    private static String propertiesName = "";
+    private static StanfordCoreNLP stanfordCoreNLP;
+    
+    private Pipeline(){
+        
+    }
+    
+    public static StanfordCoreNLP getPipeline(){
+}
